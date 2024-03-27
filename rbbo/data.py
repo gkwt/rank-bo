0a287e2ab32f3aaafc2689c3877324f44dc7c37e
Samantha Corapi <s.corapi@mail.utoronto.ca>
2024-03-27 12:45:25 -0400

    create descriptor dataset

--- rbbo/data.py
+++ rbbo/data.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import Dataset
 import pandas as pd
 
-class CustomSmilesDataset(Dataset):
+class SmilesDataset(Dataset):
     def __init__(self, smiles_data):
         self.data = pd.read_csv(smiles_data)
         self.smiles = self.data.loc['smiles']
@@ -15,3 +15,9 @@ class CustomSmilesDataset(Dataset):
         smiles = self.smiles.iloc[idx]
         target = self.target.iloc[idx]
         return smiles, target
+
+class DescriptorDataset(SmilesDataset):
+    def __init__(self, smiles, target):
+        super().__init__(smiles, target)
+
+    def get_descriptors(self)
