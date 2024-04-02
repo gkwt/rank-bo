c293bbed81cdfc286f0182eba148f5a68248ab54
Gary Tom <gary.tom@mail.mcgill.ca>
2024-04-01 21:47:11 -0400

    Updated scripts

--- sandbox/analysis.py
+++ sandbox/analysis.py
@@ -2,21 +2,20 @@ import sys
 sys.path.append('..')
 
 import pickle
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from rbbo.metrics import frac_top_n, top_one
-
+from rbbo.metrics import frac_top_n, top_one, frac_top_n_percent
 
 def read_bo_pickle(filename):
     return pickle.load(open(filename, 'rb'))
 
-
 if __name__ == '__main__':
-    dataset_name = 'CHEMBL2971_Ki_min_ROGI'
+    dataset_name = 'CHEMBL4616_EC50_max_ROGI'
     goal = 'minimize'
-    top_n = 20
+    top_n = 10
 
     work_dir = f'{dataset_name}_{goal}'
 
@@ -26,10 +25,13 @@ if __name__ == '__main__':
 
     # get the optimizations
     df_all = []
-    for loss_type in ['mse', 'random']: #['mse','ranking', 'random']:
+    for loss_type in ['mse', 'ranking', 'random']:
         if loss_type == 'random':
             # make results for a random optimization
-            bo_results = [dataset_df.sample(n_evals, ignore_index=True).reset_index() for _ in range(n_runs)]
+            bo_results = []
+            for _ in range(n_runs):
+                np.random.seed(_)
+                bo_results.append(dataset_df.sample(n_evals, ignore_index=True).reset_index())
         else:
             bo_results = read_bo_pickle(f'{work_dir}/results_{loss_type}_{dataset_name}_{goal}.pkl')
             n_runs = len(bo_results)
@@ -38,22 +40,29 @@ if __name__ == '__main__':
         for i, df in enumerate(bo_results):
             df['run'] = i
             df['loss_type'] = loss_type
-            df = frac_top_n(dataset_df, df, top_n, goal)
+            # df = frac_top_n(dataset_df, df, top_n, goal)
+            df = frac_top_n_percent(dataset_df, df, top_n, goal)
             df = top_one(df, goal)
         
         # combine the dataset
-        import pdb; pdb.set_trace()
         bo_results = pd.concat(bo_results).reset_index()
         df_all.append(bo_results)
 
     df_all = pd.concat(df_all, ignore_index=True)
 
-    sns.lineplot(df_all, x='index', y='top1', hue='loss_type', hue_order=['mse', 'ranking', 'random'])
+    sns.lineplot(df_all, x='index', y='top_one', hue='loss_type', hue_order=['mse', 'ranking', 'random'])
+    plt.xlabel('Evaluations')
+    plt.ylabel(f'Top1 fitness')
+    plt.xlim([0,n_evals-1])
+    plt.hlines(best_in_dataset, 0, n_evals, 'k', '--')
     plt.ylim([0,2]) # you may have to change this depending on the analysis
     plt.savefig(f'{dataset_name}_{goal}/{dataset_name}_top1.png')
     plt.close()
 
-    sns.lineplot(df_all, x='index', y='fracs', hue='loss_type', hue_order=['mse', 'ranking', 'random'])
+    sns.lineplot(df_all, x='index', y='frac_top_n_percent', hue='loss_type', hue_order=['mse', 'ranking', 'random'])
+    plt.xlabel('Evaluations')
+    plt.ylabel(f'Fraction of top {top_n}% of dataset')
+    plt.xlim([0,n_evals-1])
     plt.ylim([0,1])
     plt.savefig(f'{dataset_name}_{goal}/{dataset_name}_fracs{top_n}.png')
     plt.close()
--- sandbox/main.py
+++ sandbox/main.py
@@ -11,25 +11,38 @@ from rbbo.data import MoleculeDataset
 from rbbo.bo import BayesOptCampaign
 from rbbo.models import MLP, get_loss_function
 
-if __name__ == "__main__":
-    # input parameters
-    dataset_name = 'CHEMBL2971_Ki_min_ROGI'
-    goal = 'minimize'
-    loss_type = 'mse' # or ranking/mse
+from argparse import ArgumentParser
 
-    num_workers = 20
-    num_runs = 20
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--dataset_name", action="store", type=str, help="Protein target, defaults 1OYT.")
+    parser.add_argument("--num_workers", action="store", type=int, default=1, help="Number of workers, defaults 1.")
+    parser.add_argument("--num_runs", action="store", type=int, dest="num_runs", help="Number of runs in BO. Defaults 20.", default=20)
+    parser.add_argument("--num_init", action="store", type=int, dest="num_init", help="Number of initial samples defaults 20.", default=20)
+    parser.add_argument("--maximize", action="store_true", dest="goal", help="Set goal to maximize. Otherwise will minimize.", default=False)
+    parser.add_argument("--rank", action="store_true", help="Toggle use of ranking loss. Otherwise, MSE.", default=False)
+    FLAGS = parser.parse_args()
+
+    # input parameters
+    dataset_name = FLAGS.dataset_name
+    goal = 'maximize' if FLAGS.maximize else 'minimize'
+    loss_type = 'rank' if FLAGS.rank else 'mse' 
+    num_workers = FLAGS.num_workers
+    num_runs = FLAGS.num_runs
+    num_init = FLAGS.num_init
     work_dir = f'{dataset_name}_{goal}'
 
     dataset_path = f'../data/{dataset_name}.csv'
     dataset = MoleculeDataset(dataset_path, "fp", num_workers=num_workers)
-    bo = BayesOptCampaign(dataset, goal, loss_type, verbose=False, work_dir=work_dir)
-
-    # get best
-    if goal == 'maximize':
-        best_in_dataset = dataset.data.target.max()
-    elif goal == 'minimize':
-        best_in_dataset = dataset.data.target.min()
+    bo = BayesOptCampaign(
+        dataset, 
+        goal, 
+        loss_type, 
+        budget=num_runs, 
+        num_init_design = num_init, 
+        verbose=False, 
+        work_dir=work_dir
+    )
 
     # perform the run
     with multiprocessing.Pool(num_workers) as pool:
